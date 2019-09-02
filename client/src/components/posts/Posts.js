0a7fae4f6ea92421bba46895c8bfdc99cbe14b54
Adnan Reza <adnan.reza@outlook.com>
2019-09-02 10:44:08 -0700

    Implemented like/unlike functionality in front end

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import PostItem from './PostItem';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
 
@@ -9,7 +10,22 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     getPosts();
   }, [getPosts]);
 
-  return <div></div>;
+  return loading ? (
+    <Spinner></Spinner>
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome to the community
+      </p>
+      {/** Post Form */}
+      <div className='posts'>
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
